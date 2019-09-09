f73b4d8fbcb57c4ee7ec9b00fb7e206e13e36063
Okley Julio-Andreotti Papafio <julioandre275@gmail.com>
2019-09-09 05:21:37 +0000

    Finishing Touches and Preloading video on startup

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,19 @@ import VideoDetail from './VideoDetail'
 
 class App extends React.Component{
     state ={videos: [],selectedVideo: null};
+    componentDidMount(){
+        this.onTermSubmit('cars');
+    }
     onTermSubmit =async (term)=>{
        const response= await youtube.get('/search',{
             params:{
                 q:term
             }
         });
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     }
     onVideoSelect = (video) =>{
         this.setState({selectedVideo:video});
